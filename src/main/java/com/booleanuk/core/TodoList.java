88a07d572a3f6626f38d11d0d35ec662e4d90bde
ansta1994 <anton.stahlgren@hotmail.com>
2024-08-15 10:42:21 +0200

    - Added the add method, Green

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,6 +1,18 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+
 public class TodoList {
 
+    ArrayList<Task> Todo;
+
+    public TodoList() {
+        this.Todo = new ArrayList<>();
+    }
+
+    public boolean add(Task task) {
+        Todo.add(task);
+        return Todo.contains(task);
+    }
 
 }
